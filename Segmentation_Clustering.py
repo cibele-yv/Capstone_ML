192de610ca684cb22eb54ff931e5020c46715b6a
Cibele Yamanaka Vieira <cibele.yv@gmail.com>
2019-12-16 11:44:02 +0000

    adding a new Segmentation and Clustering python file

---
+++ Segmentation_Clustering.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Segmentation and Clustering
+
+# Segmentation and Clustering Machine Learning algorithms applied into a different neighborhoods of Toronto city, using the geographical coordinates of the center of each neighborhood.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
